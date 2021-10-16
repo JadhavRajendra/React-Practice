e1d6135ced67784670c4f2e0df7d4c970e215511
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-10-16 18:48:41 +0530

    button add from bootstrap

--- src/ClassComponent/BootsrapButtonExample.jsx
+++ src/ClassComponent/BootsrapButtonExample.jsx
@@ -1,48 +1,18 @@
 import React, { Component } from 'react';
-import { Carousel} from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import "./BootsrapButtonExample.css";
 
 export default class BootsrapButtonExample extends Component {
     render() {
         return (
             <div>
-             <Carousel className="main-carsoul">
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/tnc_16935516.jpg?crop=0,235,4000,2200&wid=4000&hei=2200&scl=1.0"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://images.news18.com/ibnlive/uploads/2021/07/1627377451_nature-1600x900.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+              <Button variant="primary">Primary</Button>{' '}
+              <Button variant="secondary">Secondary</Button>{' '}
+              <Button variant="success">Success</Button>{' '}
+              <Button variant="warning">Warning</Button>{' '}
+              <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+              <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+              <Button variant="link">Link</Button>
            
             </div>
         )
