0415dcf88d1a47bb27953df866e60bf761175838
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-10-24 21:13:17 +0530

    statefunction

--- src/FunctionalComponent/Statefunctionname.jsx
+++ src/FunctionalComponent/Statefunctionname.jsx
@@ -2,19 +2,20 @@ import React, { useState } from 'react'
 
     const Statefunctionname =() => {
         const [data,setData] =useState({firstName:"shoeb",lastName:"khan"});
+    
         console.log("checking===>",data)
 
         var kuchBhi =()=>{
             setData({
                 firstName :"rajendra",
-                lastName: "Jadhav"
+                lastName: "Jadhav",
             })
         }
- 
     return (
         <div>
             <h1> {data.firstName} {data.lastName}</h1>
-            <button onClick={kuchBhi} > hallo </button> 
+            <button onClick={kuchBhi} > hallo </button>
+             
           
                </div>
     );
