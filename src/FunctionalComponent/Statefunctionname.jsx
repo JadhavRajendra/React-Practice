424334c517a5359738b62a2cfc24bab68f4de05e
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-10-25 18:09:00 +0530

    Update Statefunctionname.jsx

--- src/FunctionalComponent/Statefunctionname.jsx
+++ src/FunctionalComponent/Statefunctionname.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 
     const Statefunctionname =() => {
-        const [data,setData] =useState({firstName:"shoeb",lastName:"khan"});
+        const [data,setData] =useState({firstName:"shoeb",lastName:"khan",});
+
     
         console.log("checking===>",data)
 
@@ -9,13 +10,23 @@ import React, { useState } from 'react'
             setData({
                 firstName :"rajendra",
                 lastName: "Jadhav",
+                firstname:"raj",
+                lastname: "rathod", 
+           
             })
         }
+       var changename =()=>{
+           setData({
+            firstname:"raj",
+            lastname: "rathod", 
+           })
+       }
     return (
         <div>
             <h1> {data.firstName} {data.lastName}</h1>
             <button onClick={kuchBhi} > hallo </button>
-             
+            <h1> {data.firstName} {data.lastName}</h1>
+            <button onClick={changename} > buttom </button>
           
                </div>
     );
