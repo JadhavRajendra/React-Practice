e6119fa3055742d829926180d2f7b183cb5cebcb
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-10-28 22:07:42 +0530

    Update Usestatefunction.jsx

--- src/FunctionalComponent/Usestatefunction.jsx
+++ src/FunctionalComponent/Usestatefunction.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react"
 
 const Usestatefunction =()=>{
-    const [data, SetData] = useState ( {color :"red ,blue"})
+    const [data, SetData] = useState ( {color :"red "})
  
 const setcolor =()=>{
     SetData({
-    color : "blue",
-    color: "green"
-    })
+    color : "green",
+    
+    }) 
 
 } 
     return (
 
         <div style={data}>
          
-            <h1> hello  shoeb</h1>
+            <h1> hello  shoeb </h1>
+            <p> hallo rajendra</p>
             <button onClick={setcolor}>change color</button>
         </div>
         
