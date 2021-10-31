a51506b62ee32724dec5746dff7efe9737f5323c
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-10-31 21:21:52 +0530

    Update Buttonfunctionexzample.jsx

--- src/FunctionalComponent/Buttonfunctionexzample.jsx
+++ src/FunctionalComponent/Buttonfunctionexzample.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 function Buttonfunctionexzample() {
-    const arr1 =[{name:"shoeb",age:22,address:"nanded"},{name:"Rajendra",age:23,address:"Mudkhed"},{name:"ashok",age:34,address:"shodko"}
-                , {name:"youraj",age:15,address:"danegoan"},{name:"shubam",age:21,address:"rohipimplgoun tanda"}]
+    const arr1 =[{name:"shoeb",age:22,address:"nanded"},
+                  {name:"Rajendra",age:23,address:"Mudkhed"},
+                  {name:"ashok",age:34,address:"shodko"},
+                  {name:"youraj",age:15,address:"danegoan"},
+                  {name:"shubam",age:21,address:"rohipimplgoun tanda"},
+                  {name:"vijay",age:26,address:"mudhked"} ]
     console.log(arr1)
     return (
         <div>
