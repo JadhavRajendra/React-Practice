276e087b2e73a5d455f7ae155d4728b60aecd60a
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-03 22:16:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function app(){
        {/* <BootsrapAlerts/> */}
        {/* <BootsrapAccordion/> */}
        {/* <BootsrapBadge/> */}
-       {/* <BootsrapButtonGroup/> */}
+       <BootsrapButtonGroup/>
        <BootsrapForms/>
        {/* <BootsrapToltips/> */}
        {/* <BootsrapModal/> */}
