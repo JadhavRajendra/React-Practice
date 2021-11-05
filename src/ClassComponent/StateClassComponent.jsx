d800b2b9bc9c6d369d8e5c328900d5015d46bb23
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-05 19:36:43 +0530

    Update StateClassComponent.jsx

--- src/ClassComponent/StateClassComponent.jsx
+++ src/ClassComponent/StateClassComponent.jsx
@@ -1,18 +1,22 @@
 
 import React, { Component  } from 'react'
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 export default class stateclasscomponent extends Component {
     constructor(){
         super();
         this.state= {
             name:'youraj'
+            
         }
+        
     }
     render() {
         return (
             <div>
                 <h1>hallo {this.state.name}</h1>
-            </div>
+                <button onClick ={this.setState.name}>changebutton</button>
+                            </div>
         );
     }
 }
