4b1964b8e25eff78b1df0741ed563e3b010d7ceb
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-17 16:31:51 +0530

    Update FragmentExample.jsx

--- src/FunctionalComponent/FragmentExample.jsx
+++ src/FunctionalComponent/FragmentExample.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function FragmentExample() {
     const array = [{name:'shoeb'},{name:'raj'},{name:'riyaz'},{name:'ashok'},{name:'ashok'}, 
     {name:'youraj'},{name:'vijay'},{name:'sai'},{name:'irfan'},{name:'sandip'},{name:'dipak'},
-    {name:'shrikant'},{name:'kamal'},{name: 'kamal'},{name:'rohit'},{name:'kohali'},{name:'rahul'} ]
+    {name:'shrikant'},{name:'kamal'},{name: 'kamal'},{name:'rohit'},{name:'kohali'},{name:'rahul'} ,
+    {name:'shubaham'},{name:'madhav'},{name:'akash'},{name:'ankush'},{name:'dinesh'},{name:'maroti'}]
     return (
         <>
            {array.map((item,i)=>(
