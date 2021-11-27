0da1019c135725337c9534c4a08beb225d3e0f40
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-27 22:37:05 +0530

    classprops

--- src/ClassComponent/Classpropschildexzample4.jsx
+++ src/ClassComponent/Classpropschildexzample4.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import "./Classpropschildexzample4.css"
 export default class Classpropschildexzample4 extends Component {
     render() {
