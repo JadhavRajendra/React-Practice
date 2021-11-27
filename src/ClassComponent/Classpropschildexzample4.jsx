194b01f8074fdefe1f51b726a5589a0629b520f2
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-27 22:41:21 +0530

    classpropschild

--- src/ClassComponent/Classpropschildexzample4.jsx
+++ src/ClassComponent/Classpropschildexzample4.jsx
@@ -3,7 +3,7 @@ import "./Classpropschildexzample4.css"
 export default class Classpropschildexzample4 extends Component {
     render() {
         return (
-            <div>
+            <div className="my-img">
                  <img src={this.props.pic} alt="pic" className="card-image"/>
                 <h3> produtc Name:-{this.props.name}</h3>
                 <p> price:-{this.props.price}</p>
