1d9182c00967721f912501e1b95b03644689d7ad
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-28 22:17:24 +0530

    Update Classpropsparentexzample4.jsx

--- src/ClassComponent/Classpropsparentexzample4.jsx
+++ src/ClassComponent/Classpropsparentexzample4.jsx
@@ -5,7 +5,7 @@ export default class Classpropsparentexzample4 extends Component {
     render() {
         return (
             <div className="my-app">
-               <Classpropschildexzample4 name="my car" price={400000} pic="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B07G5J5FYP._SY116_CB667322346_.jpg"/>
+               <Classpropschildexzample4 name="my car" price={400000} pic="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/WRS-Jun/PC_QC_186/Comp-186/7-min._SY116_CB666463598_.jpg"/>
                  <Classpropschildexzample4 name="Flower" price={5550} pic="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/WRS-Jun/PC_QC_186/Comp-186/3-min._SY116_CB666463598_.jpg"/>
                  <Classpropschildexzample4 name="Shoes Nike" price={430} pic="https://m.media-amazon.com/images/I/816FGXOZEXS._UL1500_.jpg"/>
                  
