a08cd9741ad9b91aab34351a90e58f1fb7f71e97
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-11-30 22:00:52 +0530

    class props

--- src/ClassComponent/Classpropschildexzample4.jsx
+++ src/ClassComponent/Classpropschildexzample4.jsx
@@ -4,7 +4,9 @@ export default class Classpropschildexzample4 extends Component {
     render() {
         return (
             <div className="my-img">
-                
+                 <img src={this.props.pic} alt="pic" className="card-image"/>
+                <h1> produtc Name :{this.props.name}</h1>
+                <p> price :{this.props.price}</p>
 
                 
             </div>
--- src/ClassComponent/Classpropsparentexzample4.jsx
+++ src/ClassComponent/Classpropsparentexzample4.jsx
@@ -5,7 +5,8 @@ export default class Classpropsparentexzample4 extends Component {
     render() {
         return (
             <div className="my-app">
-               
+               <Classpropschildexzample4 name="my car" price={400000} pic="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/WRS-Jun/PC_QC_186/Comp-186/7-min._SY116_CB666463598_.jpg"/>
+                
                  
             </div>
         )
