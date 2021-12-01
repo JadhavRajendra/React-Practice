9b948f7180765c587fd47b071eef8f65471ff75a
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2021-12-01 22:11:00 +0530

    clasprops child

--- src/ClassComponent/Classpropsparentexzample4.jsx
+++ src/ClassComponent/Classpropsparentexzample4.jsx
@@ -4,8 +4,7 @@ import "./Classpropschildexzample4.css"
 export default class Classpropsparentexzample4 extends Component {
     render() {
         return (
-            <div className="my-app">
-    <Classpropschildexzample4 name='raj' price={4000}/> 
+            <div > 
                 
                  
             </div>
